c9a4e5319c4dec681382a0b217145befb63a4178
mattiaottoborgo@gmail.com <mattiaottoborgo@gmail.com>
2023-03-30 15:04:55 +0200

    fixed index.js

--- index.js
+++ index.js
@@ -7,8 +7,5 @@ const app = express();
 // Establish port
 const port = process.env.PORT || 8080;
 app.get('/', function(request, response){
-    response.sendFile('index.html');
+    response.sendFile(path.join('index.html'));
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
